10ad6323c0901785f43366762708279e1f9a1490
I Putu Eddy Irawan <bli.ipei@gmail.com>
2024-09-23 09:55:55 +0700

    stow dotfiles

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration
+local config = wezterm.config_builder()
+
+config.default_prog = { "/usr/bin/fish", "-l" }
+config.font = wezterm.font("BerkeleyMono Nerd Font")
+config.font_size = 14.2
+
+-- keep adding configurations options here
+--
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+config.default_cursor_style = "BlinkingBlock"
+config.window_close_confirmation = "NeverPrompt"
+
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+return config
